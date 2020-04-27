8abbe81e27f885ae57d26a45b13567f968d59af2
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-27 16:30:57 -0300

    feat(exception): adding new exception to handler

--- src/main/java/br/com/customerservice/exception/handler/ExceptionHandlerController.java
+++ src/main/java/br/com/customerservice/exception/handler/ExceptionHandlerController.java
@@ -1,9 +1,18 @@
 package br.com.customerservice.exception.handler;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import br.com.customerservice.exception.CustomerNotExistsException;
 
 @RestControllerAdvice
 public class ExceptionHandlerController {
 	
+    @ExceptionHandler(CustomerNotExistsException.class)
+    public ResponseEntity<Object> handleCustomerNotExistsException() {
+        return ResponseEntity.noContent().build();
+    }
+    
 
 }
