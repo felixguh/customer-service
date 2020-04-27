fe4e07e75bddb94c849362bf0f8fd1d002123bbc
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-27 16:23:23 -0300

    feat(service): create method to create a customer

--- src/main/java/br/com/customerservice/service/CustomerService.java
+++ src/main/java/br/com/customerservice/service/CustomerService.java
@@ -2,6 +2,9 @@ package br.com.customerservice.service;
 
 import org.springframework.stereotype.Service;
 
+import br.com.customerservice.model.mapper.CustomerMapper;
+import br.com.customerservice.model.payload.CustomerPayload;
+import br.com.customerservice.model.response.CustomerResponse;
 import br.com.customerservice.repository.CustomerRepository;
 
 @Service
@@ -11,6 +14,12 @@ public class CustomerService {
 
 	public CustomerService(final CustomerRepository repository) {
 		this.repository = repository;
+	}
+
+	public CustomerResponse create(final CustomerPayload payload) {
+		final var entity = CustomerMapper.toEntity(payload);
+
+		return CustomerResponse.builder().entity(repository.save(entity)).build();
 	}
 
 }
