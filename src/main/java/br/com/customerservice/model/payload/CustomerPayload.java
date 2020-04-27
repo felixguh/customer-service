4b0ccb25279835bc3d0300141360717a281c61c0
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-27 16:57:31 -0300

    feat(model): adding annotations

--- src/main/java/br/com/customerservice/model/payload/CustomerPayload.java
+++ src/main/java/br/com/customerservice/model/payload/CustomerPayload.java
@@ -1,5 +1,9 @@
 package br.com.customerservice.model.payload;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import br.com.customerservice.util.validator.CompoundName;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,9 +14,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class CustomerPayload {
-	
+
+	@NotBlank(message = "{CustomerPayload.name.notBlank}")
+	@CompoundName(message = "{CustomerPayload.name.invalid}")
 	private String name;
-	
+
+	@NotBlank(message = "{CustomerPayload.email.notBlank}")
+	@Email(message = "{CustomerPayload.email.invalid}")
 	private String email;
-	
+
 }
