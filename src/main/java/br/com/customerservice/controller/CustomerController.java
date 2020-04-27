b9c58514cd2fd9048c547fda0a69150a3a48cd78
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-27 16:29:26 -0300

    feat(controller): create method to find customer by customerNumber

--- src/main/java/br/com/customerservice/controller/CustomerController.java
+++ src/main/java/br/com/customerservice/controller/CustomerController.java
@@ -1,9 +1,15 @@
 package br.com.customerservice.controller;
 
+import static org.springframework.http.HttpStatus.OK;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -21,6 +27,7 @@ public class CustomerController implements CustomerApi {
 		this.service = service;
 	}
 
+	@PostMapping
 	@Override
 	public ResponseEntity<CustomerResponse> create(@RequestBody final CustomerPayload payload) {
 		final var response = service.create(payload);
@@ -32,9 +39,11 @@ public class CustomerController implements CustomerApi {
 
 	}
 
+	@GetMapping("/customer/{customerNumber}")
+	@ResponseStatus(code = OK)
 	@Override
 	public CustomerResponse findByCustomerNumber(@PathVariable Long customerNumber) {
-		return null;
+		return service.findByCustomerNumber(customerNumber);
 	}
 
 }
