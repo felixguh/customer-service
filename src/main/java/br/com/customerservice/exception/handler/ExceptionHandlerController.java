31a36f1cb179a7d6ab71cae9a33733e714506d0c
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-27 16:31:50 -0300

    feat(exception): adding message source to handler

--- src/main/java/br/com/customerservice/exception/handler/ExceptionHandlerController.java
+++ src/main/java/br/com/customerservice/exception/handler/ExceptionHandlerController.java
@@ -9,6 +9,12 @@ import br.com.customerservice.exception.CustomerNotExistsException;
 @RestControllerAdvice
 public class ExceptionHandlerController {
 	
+	 private final MessageSource messageSource;
+
+	    public ExceptionHandlerController(final MessageSource messageSource) {
+	        this.messageSource = messageSource;
+	    }
+	
     @ExceptionHandler(CustomerNotExistsException.class)
     public ResponseEntity<Object> handleCustomerNotExistsException() {
         return ResponseEntity.noContent().build();
