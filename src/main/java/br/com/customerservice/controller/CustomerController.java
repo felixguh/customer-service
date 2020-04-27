38495e0efa9286c64d0f0b9b164f2232514ea9a6
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-27 16:23:39 -0300

    feat(controller): adding implementation to create a customer

--- src/main/java/br/com/customerservice/controller/CustomerController.java
+++ src/main/java/br/com/customerservice/controller/CustomerController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.customerservice.model.payload.CustomerPayload;
 import br.com.customerservice.model.response.CustomerResponse;
@@ -13,16 +14,22 @@ import br.com.customerservice.service.CustomerService;
 @RestController
 @RequestMapping("/v1/customers")
 public class CustomerController implements CustomerApi {
-	
+
 	private final CustomerService service;
-	
+
 	public CustomerController(final CustomerService service) {
 		this.service = service;
 	}
 
 	@Override
 	public ResponseEntity<CustomerResponse> create(@RequestBody final CustomerPayload payload) {
-		return null;
+		final var response = service.create(payload);
+
+		final var uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/customer/{customerNumber}")
+				.buildAndExpand(response.getCustomerNumber()).toUri();
+
+		return ResponseEntity.created(uri).body(response);
+
 	}
 
 	@Override
