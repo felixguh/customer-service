bb7b76f3b73237da04147372fa196419c209f057
Gustavo <gustavo.santosfelix@gmail.com>
2020-04-27 16:28:05 -0300

    feat(exception): creating exception when a customer is not found

---
+++ src/main/java/br/com/customerservice/exception/CustomerNotExistsException.java
@@ -0,0 +1,11 @@
+package br.com.customerservice.exception;
+
+public class CustomerNotExistsException extends RuntimeException {
+
+	private static final long serialVersionUID = -7737090142217132178L;
+	
+	public CustomerNotExistsException() {
+		super();
+	}
+
+}
